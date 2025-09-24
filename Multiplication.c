397de403749fcfce4240283b838441ce4f59b62a
lawliet-suyash <suyashbadri@gmail.com>
2025-09-24 14:17:09 +0530

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//lets write a multiplicator code
+int main(){
+    int a,b;
+    //Assigning the first Integer
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    //Assigning the second integer
+    printf("Enters second number: ");
+    scanf("%d", &b);
+    //printing the output
+    printf("The Multiplication of %d and %d is %d.\n", a, b, a*b);
+    return 0;
+}
+//End of the code
